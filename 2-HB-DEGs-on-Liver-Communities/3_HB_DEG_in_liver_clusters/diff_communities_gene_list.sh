0d06821a3161445d1401973ab93fc7ed027cacdf
Mar Batlle <marbatlleperez@gmail.com>
2021-05-20 10:03:54 +0200

    add task 2

---
+++ 2-HB-DEGs-on-Liver-Communities/3_HB_DEG_in_liver_clusters/diff_communities_gene_list.sh
@@ -0,0 +1,11 @@
+mkdir -p 3_HB_DEG_in_liver_clusters/clusters/
+mkdir -p 3_HB_DEG_in_liver_clusters/output/
+rm -f 3_HB_DEG_in_liver_clusters/output/DEG_in_clusters.txt
+for clusterid in $(cat 2_FindMolTiCommunities/liver_ppi | grep "ClusterID:" | cut -d"|" -f1 | sed "s:ClusterID\:::")
+do
+    cat 2_FindMolTiCommunities/liver_ppi | sed -n -e "/ClusterID:${clusterid}||/,/ClusterID*/ p" | sed -e '1d;$d' > 3_HB_DEG_in_liver_clusters/clusters/cluster_genes.txt
+    echo "** Cluster ${clusterid} **" >> 3_HB_DEG_in_liver_clusters/output/DEG_in_clusters.txt
+    python 3_HB_DEG_in_liver_clusters/find_genes_in_communities.py >> 3_HB_DEG_in_liver_clusters/output/DEG_in_clusters.txt
+done
+
+rm -r 3_HB_DEG_in_liver_clusters/clusters
---
+++ 2-HB-DEGs-on-Liver-Communities/3_HB_DEG_in_liver_clusters/find_genes_in_communities.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+DEG = pd.read_csv('3_HB_DEG_in_liver_clusters/HB_db_DEG_large.csv',sep=',')
+DEG_list = DEG.gene.tolist()
+
+community = pd.read_csv('3_HB_DEG_in_liver_clusters/clusters/cluster_genes.txt', header=None)
+community_list = community[0].tolist()
+
+matches = list(set(DEG_list ).intersection(set(community_list)))
+
+if len(matches) == 0:
+    print('No gene matches')
+else:
+    print(len(matches),'genes match: ',matches)
