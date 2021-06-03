4ab7bfc8371fc38d27848262e019f8839ed0ff96
Mar Batlle <marbatlleperez@gmail.com>
2021-06-03 12:10:12 +0200

    full matrix

--- 2-HB-DEGs-on-Liver-Communities/1_LiverNetworks/Network_EDA/dRWR.R
+++
@@ -1,9 +0,0 @@
-
-library(igraph)
-library(dnet)
-
-net <- erdos.renyi.game(12000, 0.3, type=c("gnp", "gnm"),
-        directed = FALSE, loops = FALSE)
-PTmatrix <- dRWR(net, normalise='laplacian', restart=0.75, normalise.affinity.matrix='quantile')
-
-print(PTmatrix[1:5,1:5])
--- 2-HB-DEGs-on-Liver-Communities/1_LiverNetworks/Network_EDA/dRWR_liver.R
+++ 2-HB-DEGs-on-Liver-Communities/1_LiverNetworks/Network_EDA/dRWR_liver.R
@@ -6,27 +6,19 @@ library(network)
 #https://www.jessesadler.com/post/network-analysis-with-r/
 # https://eehh-stanford.github.io/SNA-workshop/data-import.html#importing-an-edgelist
 
-setwd("~/Documents/TFM/GitHub/HB_PublicData/2-HB-DEGs-on-Liver-Communities/1_LiverNetworks")
+# Getting the path of your current open file
 
 # importing the edgelist
-liver.edges <- read_tsv('liver_PPI.txt', col_names = FALSE )
-
-## subsetting first 1000 edges
-liver.edges = head(liver.edges, 100000)
+liver.edges <- read.table('liver_PPI.txt')
 
 ## convert to an igraph network
 liver.edges.mat <- as.matrix(liver.edges)
 liver.net <- graph_from_edgelist(liver.edges.mat, directed=FALSE)
 
-
-#extract first names from list of names to make nicer labels. 
-firsts <- unlist(lapply(strsplit(V(liver.net )$name,  " "), '[[', 1))
-
-
-
 #let's take a look
-# plot(liver.net, vertex.shape="none", vertex.label.cex=0.6, edge.arrow.size=0.4, vertex.label=firsts, layout=layout.kamada.kawai)
+# plot(liver.net)
 
 PTmatrix <- dRWR(liver.net, normalise='laplacian', restart=0.75, normalise.affinity.matrix='quantile')
 
 print(PTmatrix[1:5,1:5])
+write.table(as.matrix(PTmatrix),'PTmatrix_liver.txt',  sep="\t")
