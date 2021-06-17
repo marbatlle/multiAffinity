aadbc9de34d23cc3e443e627a936676c07acb66b
Mar Batlle <marbatlleperez@gmail.com>
2021-06-17 13:45:47 +0200

    add_metab_network

---
+++ 2-HB-DEGs-on-Liver-Communities/1_Networks/1_PPI_Networks/scripts/dRWR_liver.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(igraph)
+library(dnet)
+library(network)
+
+#https://www.jessesadler.com/post/network-analysis-with-r/
+# https://eehh-stanford.github.io/SNA-workshop/data-import.html#importing-an-edgelist
+
+# Getting the path of your current open file
+
+# importing the edgelist
+liver.edges <- read.table('2-HB-DEGs-on-Liver-Communities/1_PPI_Networks/tmp_PPI.txt')
+
+## convert to an igraph network
+liver.edges.mat <- as.matrix(liver.edges)
+liver.net <- graph_from_edgelist(liver.edges.mat, directed=FALSE)
+
+
+PTmatrix <- dRWR(liver.net, normalise='laplacian', restart=0.5, normalise.affinity.matrix='none')
+
+write.table(as.matrix(PTmatrix),'2-HB-DEGs-on-Liver-Communities/1_PPI_Networks/PTmatrix_tmp_PPI.txt', sep="\t")
