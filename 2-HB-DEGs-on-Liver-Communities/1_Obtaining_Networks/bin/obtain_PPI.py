c395d075b3662bed6ea3d0f837fe0d42cc442cbf
Mar Batlle <marbatlleperez@gmail.com>
2021-06-19 16:54:18 +0200

    obtain_ppi

---
+++ 2-HB-DEGs-on-Liver-Communities/1_Obtaining_Networks/bin/obtain_PPI.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+#loading the human annotated PPIs database from http://iid.ophid.utoronto.ca/
+data = pd.read_csv('2-HB-DEGs-on-Liver-Communities/1_Obtaining_Networks/source/human_annotated_PPIs.txt', sep='\t')
+
+#liver interactions
+
+## select liver interactions
+liver_int = data.loc[data['liver'] != 0]
+
+##subset gene symbols
+liver_int_subset = liver_int[["symbol1","symbol2"]]
+liver_int_subset.to_csv('2-HB-DEGs-on-Liver-Communities/1_Obtaining_Networks/networks/liver_PPI.gr', sep='\t', index=False, header=None)
+
+#liver cancer interactions
+
+##select liver interactions
+liver_cancer_int = data.loc[data['liver cancer'] != 0]
+
+##subset gene symbols
+liver_cancer_int_subset = liver_cancer_int[["symbol1","symbol2"]]
+liver_cancer_int_subset.to_csv('2-HB-DEGs-on-Liver-Communities/1_Obtaining_Networks/networks/livercancer_PPI.gr', sep='\t', index=False, header=None)
+
+
---
+++ 2-HB-DEGs-on-Liver-Communities/1_Obtaining_Networks/obtain_networks.sh
@@ -0,0 +1,2 @@
+# Obtain liver and liver cancer PPI, gene interactions
+python 2-HB-DEGs-on-Liver-Communities/1_Obtaining_Networks/bin/obtain_PPI.py
