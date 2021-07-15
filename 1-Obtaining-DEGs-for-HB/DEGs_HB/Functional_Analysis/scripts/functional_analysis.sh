83f5c7c1ac8801f78f1a22580270ece1a00b9cd0
Mar Batlle <marbatlleperez@gmail.com>
2021-07-15 17:40:57 +0200

    functional analusis

---
+++ 1-Obtaining-DEGs-for-HB/DEGs_HB/Functional_Analysis/scripts/functional_analysis.sh
@@ -0,0 +1,3 @@
+# translate gene symbols to id
+#Rscript 1-Obtaining-DEGs-for-HB/DEGs_HB/Functional_Analysis/scripts/translate_names.R
+
---
+++ 1-Obtaining-DEGs-for-HB/DEGs_HB/Functional_Analysis/scripts/translate_names.R
@@ -0,0 +1,18 @@
+library('biomaRt')
+library(readr)
+
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+# Upregulated
+upregulated <- read_csv("1-Obtaining-DEGs-for-HB/DEGs_HB/HB_db_DEG_Upregulated.csv", col_types = cols_only(Name = col_guess()))
+upregulated <- upregulated$Name
+upregulated <- getBM(filters= "hgnc_symbol", attributes= c("hgnc_symbol","ensembl_gene_id","entrezgene_id"),values=upregulated,mart= mart)
+
+write.table(upregulated,file="1-Obtaining-DEGs-for-HB/DEGs_HB/Functional_Analysis/output/upregulated_translated.csv", sep="\t")
+
+# Downregulated
+downregulated <- read_csv("1-Obtaining-DEGs-for-HB/DEGs_HB/HB_db_DEG_Downregulated.csv", col_types = cols_only(Name = col_guess()))
+downregulated <- downregulated$Name
+downregulated <- getBM(filters= "hgnc_symbol", attributes= c("hgnc_symbol","ensembl_gene_id","entrezgene_id"),values=downregulated,mart= mart)
+
+write.table(downregulated, file="1-Obtaining-DEGs-for-HB/DEGs_HB/Functional_Analysis/output/downregulated_translated.csv", sep="\t")
