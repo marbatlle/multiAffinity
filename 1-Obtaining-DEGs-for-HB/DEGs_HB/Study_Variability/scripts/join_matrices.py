dd382cd97d75c974b5d739f4411c4cbb20e3e177
Mar Batlle <marbatlleperez@gmail.com>
2021-07-28 15:56:23 +0200

    study_variability

--- 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/join_matrices.py
+++ 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/join_matrices.py
@@ -2,10 +2,29 @@ import os
 import pandas as pd
 import numpy as np
 
-# Load matrices
+expression_mats  = pd.read_csv('1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/normalized_counts.txt', delimiter= "\t")
+expression_mats = expression_mats.set_index(['Unnamed: 0'])
+
+# Obtain T mean expression values
+T_exp = expression_mats.loc[:, ~expression_mats.columns.str.startswith('Normal')]
+T_exp = T_exp.mean(axis=1)
+T_exp = pd.DataFrame(T_exp, columns=['expression'])
+T_exp.index.names = ['genes']
+T_exp.drop(T_exp.loc[T_exp['expression']==0].index, inplace=True)
+T_exp = T_exp.rename(columns={'expression': "T"})
+T_exp.index.names = ['genes']
+
+# Obtain NT mean expression values
+NT_exp = expression_mats.loc[:, ~expression_mats.columns.str.startswith('Hepatoblastoma')]
+NT_exp = NT_exp.mean(axis=1)
+NT_exp = pd.DataFrame(NT_exp, columns=['expression'])
+NT_exp.index.names = ['genes']
+NT_exp.drop(NT_exp.loc[NT_exp['expression']==0].index, inplace=True)
+NT_exp = NT_exp.rename(columns={'expression': "NT"})
+NT_exp.index.names = ['genes']
+
+# Load degs
 degs = pd.read_csv('1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/degs.csv', usecols = ['Unnamed: 0','log2FoldChange'], delimiter= ",").set_index(['Unnamed: 0'])
-NT_exp = pd.read_csv('1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/NT_exp.csv', delimiter= ",").set_index(['genes'])
-T_exp = pd.read_csv('1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/T_exp.csv', delimiter= ",").set_index(['genes'])
 
 # Join matrices
 data = pd.concat([degs, NT_exp], axis=1, join='inner')
--- 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/study_variability.sh
+++ 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/study_variability.sh
@@ -1,19 +1,20 @@
 mkdir -p 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp
 
-# Create temp files
-cp 1-Obtaining-DEGs-for-HB/Matrices_HB/Originals_HB/GSE81928_raw_data.csv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/raw_data.csv
-cp 1-Obtaining-DEGs-for-HB/Metadata_HB/GSE81928_metadata.csv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/metadata.csv
+for id in $(ls 1-Obtaining-DEGs-for-HB/Metadata_HB/*_metadata.csv | sed "s:1-Obtaining-DEGs-for-HB/Metadata_HB/::" | cut -d"_" -f1)
+do
+    # Create temp files
+    cp 1-Obtaining-DEGs-for-HB/Matrices_HB/Originals_HB/${id}_raw_data.csv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/raw_data.csv
+    cp 1-Obtaining-DEGs-for-HB/Metadata_HB/${id}_metadata.csv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/metadata.csv
 
 
-# Obtain degs and mean expression
-Rscript 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/obtain_degs.R
-python 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/obtain_mean_exp.py
-python 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/join_matrices.py
-mv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/study_expression.csv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/output/GSE81928_expression.csv
+    # Obtain degs and mean expression
+    Rscript 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/obtain_degs.R
+    python 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/scripts/join_matrices.py
+    mv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/study_expression.csv 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/output/${id}_expression.csv
 
-# Remove temp files
-rm -f 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/*
-
+    # Remove temp files
+    rm -f 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp/*
+done
 
 # Remove temp folder
 rm -r -f 1-Obtaining-DEGs-for-HB/DEGs_HB/Study_Variability/tmp
