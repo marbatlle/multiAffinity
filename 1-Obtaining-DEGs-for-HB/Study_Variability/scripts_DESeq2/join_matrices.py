b72d54af4b61f52b237f2dba0136d54fcd2d5b32
Mar Batlle <marbatlleperez@gmail.com>
2021-08-08 11:49:55 +0200

    edits

--- 1-Obtaining-DEGs-for-HB/Study_Variability/scripts_DESeq2/join_matrices.py
+++ 1-Obtaining-DEGs-for-HB/Study_Variability/scripts_DESeq2/join_matrices.py
@@ -14,7 +14,7 @@ StudyA_T.drop(StudyA_T.loc[StudyA_T['Expression']==0].index, inplace=True)
 StudyA_T['Study'] = 'Study A'
 
 StudyA_T.index = [x for x in range(1, len(StudyA_T.values)+1)]
-StudyA_T = StudyA_T.sample(500)
+#StudyA_T = StudyA_T.sample(500)
 
 
 StudyA_NT = StudyA.loc[:, ~StudyA.columns.str.startswith('Hepatoblastoma')]
@@ -25,7 +25,7 @@ StudyA_NT.drop(StudyA_NT.loc[StudyA_NT['Expression']==0].index, inplace=True)
 StudyA_NT['Study'] = 'Study A'
 
 StudyA_NT.index = [x for x in range(1, len(StudyA_NT.values)+1)]
-StudyA_NT = StudyA_NT.sample(500)
+#StudyA_NT = StudyA_NT.sample(500)
 
 
 # Study B
@@ -40,7 +40,7 @@ StudyB_T.drop(StudyB_T.loc[StudyB_T['Expression']==0].index, inplace=True)
 StudyB_T['Study'] = 'Study B'
 
 StudyB_T.index = [x for x in range(1, len(StudyB_T.values)+1)]
-StudyB_T = StudyB_T.sample(500)
+#StudyB_T = StudyB_T.sample(500)
 
 StudyB_NT = StudyB.loc[:, ~StudyB.columns.str.startswith('Hepatoblastoma')]
 StudyB_NT = StudyB.mean(axis=1)
@@ -50,7 +50,7 @@ StudyB_NT.drop(StudyB_NT.loc[StudyB_NT['Expression']==0].index, inplace=True)
 StudyB_NT['Study'] = 'Study B'
 
 StudyB_NT.index = [x for x in range(1, len(StudyB_NT.values)+1)]
-StudyB_NT = StudyB_NT.sample(500)
+#StudyB_NT = StudyB_NT.sample(500)
 
 # Study c
 StudyC = pd.read_csv('1-Obtaining-DEGs-for-HB/Matrices_HB/Normalized_HB/normalized_GSE104766.txt',sep='\t')
@@ -127,8 +127,67 @@ StudyE_NT = StudyE_NT.sample(500)
 
 # NT
 data_NT = pd.concat([StudyA_NT, StudyB_NT], axis=0)
-data_NT.to_csv("1-Obtaining-DEGs-for-HB/Study_Variability/Normalization_Comparison/NT_deseq.csv", index=False)
+data_NT.to_csv("1-Obtaining-DEGs-for-HB/Study_Variability/NT_deseq.csv", index=False)
 
 # T
 data_T = pd.concat([StudyA_T, StudyB_T], axis=0)
-data_T.to_csv("1-Obtaining-DEGs-for-HB/Study_Variability/Normalization_Comparison/T_deseq.csv", index=False)
+data_T.to_csv("1-Obtaining-DEGs-for-HB/Study_Variability/T_deseq.csv", index=False)
+
+# ONLY DEGS
+
+# Import DEGs
+
+degs = pd.read_csv('1-Obtaining-DEGs-for-HB/DEGs_HB/HB_db_DEG.csv',sep=',')
+degs_list = degs['Name'].tolist()
+
+# Study A - DEGs
+StudyA = pd.read_csv('1-Obtaining-DEGs-for-HB/Matrices_HB/Normalized_HB/normalized_GSE81928.txt',sep='\t')
+StudyA = StudyA.set_index(['Unnamed: 0'])
+StudyA = StudyA[StudyA.index.isin(degs_list)]
+
+StudyA_T = StudyA.loc[:, ~StudyA.columns.str.startswith('Normal')]
+StudyA_T = StudyA_T.mean(axis=1)
+StudyA_T = pd.DataFrame(StudyA_T, columns=['Expression'])
+StudyA_T.index.names = ['genes']
+StudyA_T.drop(StudyA_T.loc[StudyA_T['Expression']==0].index, inplace=True)
+StudyA_T['Study'] = 'Study A'
+StudyA_T.index = [x for x in range(1, len(StudyA_T.values)+1)]
+
+StudyA_NT = StudyA.loc[:, ~StudyA.columns.str.startswith('Hepatoblastoma')]
+StudyA_NT = StudyA.mean(axis=1)
+StudyA_NT = pd.DataFrame(StudyA_NT, columns=['Expression'])
+StudyA_NT.index.names = ['genes']
+StudyA_NT.drop(StudyA_NT.loc[StudyA_NT['Expression']==0].index, inplace=True)
+StudyA_NT['Study'] = 'Study A'
+StudyA_NT.index = [x for x in range(1, len(StudyA_NT.values)+1)]
+
+
+# Study B - DEGs
+StudyB = pd.read_csv('1-Obtaining-DEGs-for-HB/Matrices_HB/Normalized_HB/normalized_GSE89775.txt',sep='\t')
+StudyB = StudyB.set_index(['Unnamed: 0'])
+StudyB = StudyB[StudyB.index.isin(degs_list)]
+
+StudyB_T = StudyB.loc[:, ~StudyB.columns.str.startswith('Normal')]
+StudyB_T = StudyB_T.mean(axis=1)
+StudyB_T = pd.DataFrame(StudyB_T, columns=['Expression'])
+StudyB_T.index.names = ['genes']
+StudyB_T.drop(StudyB_T.loc[StudyB_T['Expression']==0].index, inplace=True)
+StudyB_T['Study'] = 'Study B'
+StudyB_T.index = [x for x in range(1, len(StudyB_T.values)+1)]
+
+StudyB_NT = StudyB.loc[:, ~StudyB.columns.str.startswith('Hepatoblastoma')]
+StudyB_NT = StudyB.mean(axis=1)
+StudyB_NT = pd.DataFrame(StudyB_NT, columns=['Expression'])
+StudyB_NT.index.names = ['genes']
+StudyB_NT.drop(StudyB_NT.loc[StudyB_NT['Expression']==0].index, inplace=True)
+StudyB_NT['Study'] = 'Study B'
+StudyB_NT.index = [x for x in range(1, len(StudyB_NT.values)+1)]
+
+
+# NT
+data_NT = pd.concat([StudyA_NT, StudyB_NT], axis=0)
+data_NT.to_csv("1-Obtaining-DEGs-for-HB/Study_Variability/output/NT_deseq_DEGs.csv", index=False)
+
+# T
+data_T = pd.concat([StudyA_T, StudyB_T], axis=0)
+data_T.to_csv("1-Obtaining-DEGs-for-HB/Study_Variability/output/T_deseq_DEGs.csv", index=False)
