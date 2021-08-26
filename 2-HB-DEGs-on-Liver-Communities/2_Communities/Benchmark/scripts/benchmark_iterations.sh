60325935a8969ea97eecb34a837828f879354c3f
Mar Batlle <marbatlleperez@gmail.com>
2021-08-26 18:05:43 +0200

    clean

--- 2-HB-DEGs-on-Liver-Communities/2_Communities/Benchmark/scripts/benchmark_iterations.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..100}
-do
- bash 2-HB-DEGs-on-Liver-Communities/2_Communities/Benchmark/scripts/benchmark_communities.sh
- mv 2-HB-DEGs-on-Liver-Communities/2_Communities/Benchmark/output/optimal_rands_overview.doc 2-HB-DEGs-on-Liver-Communities/2_Communities/Benchmark/output/optimal_rands_overview_${i}.txt
-done
--- 2-HB-DEGs-on-Liver-Communities/2_Communities/Benchmark/scripts/synthetic_model.R
+++
@@ -1,8 +0,0 @@
-pacman::p_load(igraph)
-
-m <- 100 # the number of edges in the graph
-set.seed(64)
-g <- sample_gnm(20000, m)
-edges <- as_edgelist(g)
-
-write.table(edges, file='2-HB-DEGs-on-Liver-Communities/2_Communities/Benchmark/edges_tmp', sep='\t')
