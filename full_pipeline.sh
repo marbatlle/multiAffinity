8f58ad8f2c35e3fbd1d3748d418e05b132d8f8e0
Mar Batlle <marbatlleperez@gmail.com>
2021-08-26 19:27:39 +0200

    edit full pipeline

---
+++ full_pipeline.sh
@@ -0,0 +1,12 @@
+# 1-metaDEGs (read 1-metaDEGs/metaDEGs_info.md to fullfill input requriements - GREIN raw data)
+bash 1-metaDEGs/obtain_metaDEGs.sh
+
+## Extra analysis steps
+    ## 1a-Study Annotation (follow 1a-StudyAnnotation/annotation_info.md instructions to obtain plots)
+    ## 1b-StudyVariability (follow 1b-StudyVariability/study_variability.sh instructions to obtain plots)
+
+# 2-COMMgenes (if you you want to check optimal number randomizations, go to 2-COMMgenes/COMMgenes.md)
+bash 2-COMMgenes/obtain_COMMgenes.sh
+
+# 3-Node Affinity
+bash 3-NodeAffinity/affinity.sh
