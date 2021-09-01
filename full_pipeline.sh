eb4ac165f6ff4f59f6fa0e606f6bad0571ca773c
Mar Batlle <marbatlleperez@gmail.com>
2021-09-01 14:15:38 +0200

    metaDEGs clean

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -1,12 +1,25 @@
+
+
+
+echo 'STEP1 - Finding metaDEGs'
+mkdir -p bin/1-metaDEGs/src; mkdir -p bin/1-metaDEGs/src/grein; cp input/data/* bin/1-metaDEGs/src/grein/
+(cd bin/1-metaDEGs/; bash obtain_metaDEGs.sh)
+[ "$(ls -A bin/1-metaDEGs/output/)" ] && : || (echo "metaDEGs processes NOT COMPLETED, please check the README.md to find a solution"; exit 1) #STEP1 check
+mkdir -p output; mkdir -p output/1-metaDEGs; mv bin/1-metaDEGs/output/* output/1-metaDEGs; rm -r -f bin/1-metaDEGs/src; rm -r -f bin/1-metaDEGs/output
+
+
+
+
+
 # 1-metaDEGs (read 1-metaDEGs/metaDEGs_info.md to fullfill input requriements - GREIN raw data)
-bash 1-metaDEGs/obtain_metaDEGs.sh
+
 
 ## Extra analysis steps
     ## 1a-Study Annotation (follow 1a-StudyAnnotation/annotation_info.md instructions to obtain plots)
     ## 1b-StudyVariability (follow 1b-StudyVariability/study_variability.sh instructions to obtain plots)
 
 # 2-COMMgenes (if you you want to check optimal number randomizations, go to 2-COMMgenes/COMMgenes.md)
-bash 2-COMMgenes/obtain_COMMgenes.sh
+#bash 2-COMMgenes/obtain_COMMgenes.sh
 
 # 3-Node Affinity
-bash 3-MultiAffinity/multiAffinity.sh
+#bash 3-MultiAffinity/multiAffinity.sh
