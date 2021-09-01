7b2e04327396a34a4592bcde418304269d68c918
Mar Batlle <marbatlleperez@gmail.com>
2021-09-01 18:18:49 +0200

    edit

--- SeqAffinity.sh
+++ SeqAffinity.sh
@@ -21,5 +21,7 @@ mkdir -p output/2-COMMgenes; mv bin/2-COMMgenes/output/* output/2-COMMgenes; rm 
 echo 'STEP3 - Perform affinity study'
 cp -r output/1-metaDEGs bin/3-MultiAffinity/src; cp -r input/layers/ bin/3-MultiAffinity/src
 (cd bin/; bash 3-MultiAffinity/multiAffinity.sh)
-rm -r bin/3-MultiAffinity/src/1-metaDEGs; rm -r bin/3-MultiAffinity/src/layers
-mkdir -p output/3-MultiAffinity; mv bin/3-MultiAffinity/output/* output/3-MultiAffinity; 
+rm -r bin/3-MultiAffinity/src/1-metaDEGs; rm -r bin/3-MultiAffinity/src/layers; mkdir -p output/3-MultiAffinity; mv bin/3-MultiAffinity/output/* output/3-MultiAffinity
+
+find . -type d -empty -delete
+echo 'SeqAffinity completed. Check out the output folder'
