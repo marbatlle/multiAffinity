b3fce38f1bf19ba3c94e9c7598e58f3e6a7c9b05
Mar Batlle <marbatlleperez@gmail.com>
2021-09-01 18:12:11 +0200

    clean

--- test.sh
+++
@@ -1,5 +0,0 @@
-echo 'STEP3 - Perform affinity study'
-cp -r output/1-metaDEGs bin/3-MultiAffinity/src; cp -r input/layers/ bin/3-MultiAffinity/src
-(cd bin/; bash 3-MultiAffinity/multiAffinity.sh)
-rm -r bin/3-MultiAffinity/src/1-metaDEGs; rm -r bin/3-MultiAffinity/src/layers
-mkdir -p output/3-MultiAffinity; mv bin/3-MultiAffinity/output/* output/3-MultiAffinity; 
