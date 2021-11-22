ddaac2cecf270b6fdfcb7ca561f5ed99c158928c
marbatlle <marbatlleperez@gmail.com>
2021-11-22 17:56:57 +0100

    update sample_data

--- tool/bin/Conclude/run_output.sh
+++ tool/bin/Conclude/run_output.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+# Create output report
 python bin/Conclude/output.py
-python bin/Conclude/plot_participation.py
+
+# Create output plot
+mapfile -n 2 < output/multiAffinity_report.csv
+if ((${#MAPFILE[@]}>1)); then
+    python bin/Conclude/plot_participation.py
+fi
+
+# Organize output files
 cd output
-rm -rf metaDEGs/dif_exp
-rm -rf metaDEGs/normalized_counts
-rm -f metaDEGs/degs_names.txt
-
-rm -rf Communities/clusters
-rm -f Communities/degs_communities.txt
-
-rm -f  Affinity/part_coef.txt
-rm -f Affinity/difexp.txt
-rm -f Affinity/Affinity_Corr.txt
+rm -rf metaDEGs/dif_exp ; rm -rf metaDEGs/normalized_counts; rm -f metaDEGs/degs_names.txt ; rm -rf Communities/clusters; rm -f Communities/degs_communities.txt ; rm -f  Affinity/part_coef.txt ; rm -f Affinity/difexp.txt; rm -f Affinity/Affinity_Corr.txt
